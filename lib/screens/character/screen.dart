a7e55533829251d66e96fa0b3677c6fe12a58503
anbv731 <anbv731@yandex.ru>
2021-05-18 16:57:42 +0400

    3.5 screen

--- lib/screens/character/screen.dart
+++ lib/screens/character/screen.dart
@@ -1,33 +1,32 @@
-
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
 class Character extends StatelessWidget {
-
-@override
+  @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    body:      Container(
-        width: MediaQuery.of(context).size.width*1,
-        height: MediaQuery.of(context).size.height/4,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/rik.png'),
-            fit: BoxFit.cover,
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 4,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/rik.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+              child: Container(
+                color: Colors.black.withOpacity(0.6),
+              ),
+            ),
           ),
-        ),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 3,sigmaY: 3),
-
-          child: Container(
-            color: Colors.black.withOpacity(0.6),
-          ),
-        ),
-      )
-  );
-
-
-
+CircleAvatar(child: Image.asset('assets/images/rik.png',    fit: BoxFit.contain,))
+        ],
+      ),
+    );
   }
 }
